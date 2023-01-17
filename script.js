4d6d6452241ff53585bb350b0f4467165de71a4d
SBars <samuelbarsuhn@gmail.com>
2023-01-17 17:00:45 -0600

    added comments to the script and reformated the HTML

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // in the html.
 $(function () {
   // TODO: Add code to display the current date in the header of the page.
+
+  // This section uses dayjs to set the current date and time. It is then implemented onto the page by setting the text of the <p> element with the id of "currentDay" as the current date and time
   var currentTime = dayjs().format("ddd MMM-D-YYYY H:m");
   $("#currentDay").text(currentTime);
 
@@ -12,6 +14,8 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  // This function will take any input that is in the text boxes and put it into local storage but only if the user clicks the save button to the right of the box they want to save
   $(".saveBtn").on("click", function () {
     let boxNum = $(this).parent().attr("id");
     let textValue = $(this).siblings(".description").val();
@@ -25,7 +29,8 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  // This function checks to see if the hour in each section is in the past, present, or future. if the section is in the past it will be given the class "past" and will turn grey, if it is in the future it will turn green, and if it is the current hour it will turn red
   const currentHour = dayjs().format("H");
   $(".time-block").each(function () {
     let divId = $(this).attr("id");
@@ -42,15 +47,15 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   // need to reference hour number in get item
-  
-  $("#09 .description").val(localStorage.getItem("09"))
-  $("#10 .description").val(localStorage.getItem("10"))
-  $("#11 .description").val(localStorage.getItem("11"))
-  $("#12 .description").val(localStorage.getItem("12"))
-  $("#13 .description").val(localStorage.getItem("13"))
-  $("#14 .description").val(localStorage.getItem("14"))
-  $("#15 .description").val(localStorage.getItem("15"))
-  $("#16 .description").val(localStorage.getItem("16"))
-  $("#17 .description").val(localStorage.getItem("17"))
-  
+
+  // this section grabs the local storage for each input box. This way when the page is loaded any inputs that have been saved will appear in their corresponding box.
+  $("#09 .description").val(localStorage.getItem("09"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 });
